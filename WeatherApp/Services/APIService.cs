ffbcbb93f9255be6db6961e494b1abfa04d39792
giolga <giolga.kumi@gmail.com>
2024-09-06 16:07:37 +0400

    Images problem fixed!

--- WeatherApp/Services/APIService.cs
+++ WeatherApp/Services/APIService.cs
@@ -8,13 +8,23 @@ using WeatherApp.Models;
 
 namespace WeatherApp.Services
 {
-    public class APIService
+    public static class APIService
     {
-        public async Task<Root> GetWeather(double latitude, double longitude)
+        public static async Task<Root> GetWeather(double latitude, double longitude)
         {
             var httpClient = new HttpClient();
             // store the result int that variable. it contains JSON data
             var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=3a75f6c38bf1d813b50ac6768e28627a", latitude, longitude));
+
+            // Deserialize into C# classes   
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+
+        public static async Task<Root> GetWeatherByCity(string city)
+        {
+            var httpClient = new HttpClient();
+            // store the result int that variable. it contains JSON data
+            var response = await httpClient.GetStringAsync(string.Format("api.openweathermap.org/data/2.5/forecast?q={0}&appid=3a75f6c38bf1d813b50ac6768e28627a", city));
 
             // Deserialize into C# classes   
             return JsonConvert.DeserializeObject<Root>(response);
--- WeatherApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/WeatherApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b2aef4e7b444690e753516717f7278bca1b36368")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
